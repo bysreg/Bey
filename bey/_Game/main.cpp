51f4d154be9fa9ed57c27a29ddc3c197b84e756d
perequai <perequai@gmail.com>
2014-03-06 16:31:38 +0700

    - use unicode in _Game project - inmplement window and handle window messages

--- bey/_Game/main.cpp
+++ bey/_Game/main.cpp
@@ -1,15 +1,102 @@
 #include <windows.h>
+#include <windowsx.h>
 
-int WINAPI WinMain(HINSTANCE hInstance,
+// the WindowProc function prototype
+LRESULT CALLBACK WindowProc(HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, // an integer which identifies this application
 	HINSTANCE hPrevInstance, // obsolete
 	LPSTR lpCmdLine, // long pointer to the command line string
 	int nCmdShow) // how window appear when created
 {
-	// create a "Hello World" message box using MessageBox()
-	MessageBox(NULL,
-		"Hello World!",
-		"Just another Hello World program!",
-		MB_ICONEXCLAMATION | MB_OK);
+	// the handle for the window, filled by a function
+	HWND hWnd;
+	// this struct holds information for the window class
+	WNDCLASSEX wc;
 
-	return 0;
+	// clear out the window class for use
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	// fill in the struct with the needed information
+	wc.cbSize = sizeof(WNDCLASSEX); //  size of this structure
+	wc.style = CS_HREDRAW | CS_VREDRAW; // tell Windows to redraw the window if it is moved vertically or horizontally
+	wc.lpfnWndProc = WindowProc; //  what function to use when it gets a message from Windows
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);  // stores the default mouse image for the window class
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW; // 
+	wc.lpszClassName = L"WindowClass1";
+
+	// register the window class
+	RegisterClassEx(&wc);
+
+	RECT wr = { 0, 0, 500, 400 };    // set the client size, but not the position
+	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);    // adjust the size
+
+	// create the window and use the result as the handle
+	hWnd = CreateWindowEx(NULL,
+		L"WindowClass1",    // name of the window class
+		L"Our First Windowed Program",   // title of the window
+		WS_OVERLAPPEDWINDOW,    // window style
+		300,    // x-position of the window
+		300,    // y-position of the window
+		500,    // width of the window
+		400,    // height of the window
+		NULL,    // we have no parent window, NULL
+		NULL,    // we aren't using menus, NULL
+		hInstance,    // application handle
+		NULL);    // used with multiple windows, NULL
+
+	// display the window on the screen
+	ShowWindow(hWnd, nCmdShow);
+
+	// enter the main loop:
+
+	// this struct holds Windows event messages
+	MSG msg;
+
+	// wait for the next message in the queue, store the result in 'msg'
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			// translate keystroke messages into the right format
+			TranslateMessage(&msg);
+
+			// send the message to the WindowProc function
+			DispatchMessage(&msg);
+
+			// check to see if it's time to quit
+			if (msg.message == WM_QUIT)
+				break;
+		}
+		else
+		{
+			// Run game code here
+		}
+		
+	}
+
+	// return this part of the WM_QUIT message to Windows
+	return msg.wParam;
+}
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	// sort through and find what code to run for the message given
+	switch (message)
+	{
+		// this message is read when the window is closed
+		case WM_DESTROY:
+		{
+			// close the application entirely
+			PostQuitMessage(0);
+			return 0;
+		} break;
+	}
+
+	// Handle any messages the switch statement didn't
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
