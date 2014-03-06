68fb3c0fd5752c9878efe33b544c939b48c41c17
perequai <perequai@gmail.com>
2014-03-06 11:05:11 +0700

    + create window

--- bey/_Game/main.cpp
+++ bey/_Game/main.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
+#include <windows.h>
 
-using namespace std;
+int WINAPI WinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance, // obsolete
+	LPSTR lpCmdLine, // long pointer to the command line string
+	int nCmdShow) // how window appear when created
+{
+	// create a "Hello World" message box using MessageBox()
+	MessageBox(NULL,
+		"Hello World!",
+		"Just another Hello World program!",
+		MB_ICONEXCLAMATION | MB_OK);
 
-int main() {
-	int a;
-	cout << "Hello World!" << endl;
-	cin >> a;
 	return 0;
 }
