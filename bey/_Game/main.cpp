fb23d6f6909f5d6509598d244b9fcc76bbfda46a
perequai <perequai@gmail.com>
2014-03-06 21:11:46 +0700

    - setup for direct x

--- bey/_Game/main.cpp
+++ bey/_Game/main.cpp
@@ -1,11 +1,60 @@
 #include <windows.h>
 #include <windowsx.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
+// global declarations
+IDXGISwapChain *g_SwapChain;             // the pointer to the swap chain interface
+ID3D11Device *g_Device;                     // the pointer to our Direct3D device interface
+ID3D11DeviceContext *g_DeviceContext;           // the pointer to our Direct3D device context
+
+// function prototypes
+void InitD3D(HWND hWnd);     // sets up and initializes Direct3D
+void CleanD3D(void);         // closes Direct3D and releases memory
 
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd,
 	UINT message,
 	WPARAM wParam,
 	LPARAM lParam);
+
+// this function initializes and prepares Direct3D for use
+void InitD3D(HWND hWnd) 
+{
+	// create a struct to hold information about the swap chain
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	// clear out the struct for use
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	// fill the swap chain description struct
+	scd.BufferCount = 1;                                    // one back buffer
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+	scd.OutputWindow = hWnd;                                // the window to be used
+	scd.SampleDesc.Count = 4;                               // how many multisamples
+	scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+	// create a device, device context and swap chain using the information in the scd struct
+	D3D11CreateDeviceAndSwapChain(NULL, // use default adapter (there might be more than one graphics adapter)
+		D3D_DRIVER_TYPE_HARDWARE, // 
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&g_SwapChain,
+		&g_Device,
+		NULL,
+		&g_DeviceContext);
+}
 
 int WINAPI WinMain(HINSTANCE hInstance, // an integer which identifies this application
 	HINSTANCE hPrevInstance, // obsolete
