85aede53b6b2b0e9b76d1316b43fb0e9aa4deb67
bysreg <hilman09121991@gmail.com>
2014-03-10 08:37:30 +0700

    - use console too aside from the GUI window

--- bey/_Game/main.cpp
+++ bey/_Game/main.cpp
@@ -10,13 +10,13 @@ LRESULT CALLBACK WindowProc(HWND hWnd,
 	LPARAM lParam);
 
 void CreateMainWindow(const HINSTANCE& hInstance, HWND& hWnd, int screenWidth, int screenHeight) {
-	WNDCLASSEX wc; // this struct holds information for the window class
+	WNDCLASSEX wc; // this struct holds information for the window class (wc)
 
 	// clear out the window class for use
 	ZeroMemory(&wc, sizeof(WNDCLASSEX));
 
 	// fill in the struct with the needed information
-	wc.cbSize = sizeof(WNDCLASSEX); //  size of this structure
+	wc.cbSize = sizeof(wc); //  size of this structure
 	wc.style = CS_HREDRAW | CS_VREDRAW; // tell Windows to redraw the window if it is moved vertically or horizontally
 	wc.lpfnWndProc = WindowProc; //  what function to use when it gets a message from Windows
 	wc.hInstance = hInstance;
@@ -26,14 +26,16 @@ void CreateMainWindow(const HINSTANCE& hInstance, HWND& hWnd, int screenWidth, i
 	// register the window class
 	RegisterClassEx(&wc);
 
+	DWORD windowStyle = WS_VISIBLE | WS_POPUP | WS_BORDER | WS_SYSMENU | WS_CAPTION;
+
 	RECT wr = { 0, 0, screenWidth, screenHeight };    // set the client size, but not the position
-	AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);    // adjust the size (so that the border is also counted for the size)
+	AdjustWindowRect(&wr, windowStyle, FALSE);    // adjust the size (so that the border is also counted for the size)
 
 	// create the window and use the result as the handle
 	hWnd = CreateWindowEx(NULL,
 		L"WindowClass1",    // name of the window class
-		L"Our First Windowed Program",   // title of the window
-		WS_OVERLAPPEDWINDOW,    // window style
+		L"Game Title",   // title of the window
+		windowStyle,    // window style
 		200,    // x-position of the window
 		200,    // y-position of the window
 		wr.right - wr.left,    // width of the window (including window border)
@@ -41,9 +43,12 @@ void CreateMainWindow(const HINSTANCE& hInstance, HWND& hWnd, int screenWidth, i
 		NULL,    // we have no parent window, NULL
 		NULL,    // we aren't using menus, NULL
 		hInstance,    // application handle
-		NULL);    // used with multiple windows, NULL
+		NULL);    // used with multiple windows, NULL	
+
+	ShowWindow(hWnd, TRUE);
 }
 
+/*
 int WINAPI WinMain(HINSTANCE hInstance, // an integer which identifies this application
 	HINSTANCE hPrevInstance, // obsolete
 	LPSTR lpCmdLine, // long pointer to the command line string
@@ -89,6 +94,7 @@ int WINAPI WinMain(HINSTANCE hInstance, // an integer which identifies this appl
 	// return this part of the WM_QUIT message to Windows
 	return msg.wParam;
 }
+*/
 
 // this is the main message handler for the program
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
@@ -107,4 +113,49 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
 
 	// Handle any messages the switch statement didn't
 	return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+
+int main() {
+	HWND hWnd;
+	HINSTANCE hInstance = GetModuleHandle(NULL);;
+	int screenWidth = 800, screenHeight = 600;
+
+	CreateMainWindow(hInstance, hWnd, screenWidth, screenHeight);	
+
+	// initialize direct3D
+	bey::RenderingInitData data;
+	data.screenHeight = screenHeight;
+	data.screenWidth = screenWidth;
+	data.handleWindow = hWnd;
+	bey::Rendering::GetInstance().Init(data);	
+
+	// enter the main loop:
+
+	// this struct holds Windows event messages
+	MSG msg;
+
+	// wait for the next message in the queue, store the result in 'msg'
+	while (true)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			// translate keystroke messages into the right format
+			TranslateMessage(&msg);
+
+			// send the message to the WindowProc function
+			DispatchMessage(&msg);
+
+			// check to see if it's time to quit
+			if (msg.message == WM_QUIT)
+				break;
+		}
+		bey::Rendering::GetInstance().Render();
+	}
+
+	bey::Rendering::GetInstance().Clean();
+
+	// return this part of the WM_QUIT message to Windows
+	return msg.wParam;
+
+	return 0;
 }
